ce5860c3ae2c9602bfb363f59ece338115f9d1d5
Ania <annabulat@gmail.com>
2019-06-04 17:02:37 +0200

    zmiany z excela

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -87,20 +87,20 @@
 
         //active menu
 
-        $(function(){
-
-            var url = window.location.pathname , // in real app this would have to be replaced with window.location.pathname
-                urlRegExp = new RegExp(url.replace(/\/$/,'')); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
-
-            // now grab every link from the navigation
-            $('#nav a').each(function(){
-                // and test its href against the url pathname regexp
-                if(urlRegExp.test(this.href)){
-                    $(this).addClass('active');
-                }
-            });
-
-        });
+        // $(function(){
+        //
+        //     var url = window.location.pathname , // in real app this would have to be replaced with window.location.pathname
+        //         urlRegExp = new RegExp(url.replace(/\/$/,'')); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
+        //
+        //     // now grab every link from the navigation
+        //     $('#nav a').each(function(){
+        //         // and test its href against the url pathname regexp
+        //         if(urlRegExp.test(this.href)){
+        //             $(this).addClass('active');
+        //         }
+        //     });
+        //
+        // });
 
 
         var positionMenu = jQuery(".careers-main-drop a").offset().left;
