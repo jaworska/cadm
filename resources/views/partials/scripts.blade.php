8f84e2c7849cf7d1dba40fa6a3cd08c81dae4b79
Asia <joanna.jaworska@invisio.digital>
2019-06-11 12:32:29 +0200

    Asia praca: Rzeczy

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -659,6 +659,31 @@
             }, 800);
         });
 
+        //group play video on scroll
+
+        var media = $('#video-group').not("[autoplay='autoplay']");
+        var tolerancePixel = 40;
+
+        function checkMedia(){
+            // Get current browser top and bottom
+            var scrollTop = $(window).scrollTop() + tolerancePixel;
+            var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+
+            media.each(function(index, el) {
+                var yTopMedia = $(this).offset().top;
+                var yBottomMedia = $(this).height() + yTopMedia;
+
+                if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){
+                    $(this).get(0).play();
+                } else {
+                    $(this).get(0).pause();
+                }
+            });
+
+            //}
+        }
+        $(document).on('scroll', checkMedia);
+
     });
 
 </script>
